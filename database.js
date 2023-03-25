44a2106af1394d729dc38d0fed93823b48b9ec78
Anazeb <anam.aurangzeb@gmail.com>
2023-03-26 00:17:09 +0100

    authenticating the user and password

--- database.js
+++ database.js
@@ -5,11 +5,11 @@ const init = () => {
 
 
   db.serialize(() => {
-    db.run('CREATE TABLE users (userID int NOT NULL PRIMARY KEY, role TEXT, name TEXT, password TEXT)')
-    db.run("INSERT INTO users VALUES (1, 'student', 'Liah', 'password')")
-    db.run("INSERT INTO users VALUES (2, 'teacher', 'Hannah', '123password')")
-    db.run("INSERT INTO users VALUES (3, 'admin', 'Annie', '231password')")
-    db.run("INSERT INTO users VALUES (4, 'student', 'Abbe', 'password123')")
+    db.run('CREATE TABLE users (userID INTEGER PRIMARY KEY AUTOINCREMENT, role TEXT, name TEXT, password TEXT)')
+    db.run("INSERT INTO users ('role', 'name', 'password') VALUES ('student', 'Liah', 'password')")
+    db.run("INSERT INTO users ('role', 'name', 'password') VALUES ('teacher', 'Hannah', '123password')")
+    db.run("INSERT INTO users ('role', 'name', 'password') VALUES ('admin', 'admin', 'admin')")
+    db.run("INSERT INTO users ('role', 'name', 'password') VALUES ('student', 'Abbe', 'password123')")
   })
   db.all('SELECT * FROM users', (err, result) => {
     if (err) {
@@ -19,7 +19,55 @@ const init = () => {
       console.log(result)
     }
   })
+}
+const getName = (userName) => {
+  return new Promise((resolve, reject) => {
+
+    db.all('SELECT * FROM users WHERE name = ?', [userName], (err, result) => {
+      if (err) {
+        console.log(err)
+        reject(err)
+      }
+      else {
+        console.log(result)
+        resolve(result)
+
+      }
+    })
+
+  }
+
+  )
 
 }
-module.exports = { init }
+
+const addUserInfo = (username, password, role) => {
+  return new Promise((resolve, reject) => {
+    db.run("INSERT INTO users ( 'role', 'name','password') VALUES (?, ?, ?)", [role, username, password], (err, result) => {
+      if (err) {
+        console.log("error")
+        reject(err)
+      }
+      else {
+        console.log("inserted")
+        db.all('SELECT * FROM users', (err, result) => {
+          if (err) {
+            reject(err)
+          }
+          else {
+            console.log(result)
+          }
+        })
+        resolve(result)
+      }
+    })
+
+  }
+
+  )
+
+}
+
+
+module.exports = { init, getName, addUserInfo }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt")
 require('dotenv').config()
 app.set('view-engine, ejs')
 const db = require('./database')
@@ -17,18 +18,76 @@ app.get('/', (req, res) => {
   res.redirect('/identify')
 })
 
-app.post('/identify', (req, res) => {
-  const username = req.body.password
-  const token = jwt.sign(username, process.env.ACCESS_TOEN_SECRET)
-  currentKey = token
-  currentPassword = username
-  res.redirect('/granted')
+
+
+app.post('/identify', async (req, res) => {
+  let userName = req.body.userId
+  var password
+  var encryptedpass
+  let role = req.body.role
+  let userNameExists = false
+  if (userName != "") {
+    const rest = await db.getName(userName).then(results => {
+      if (results.length == 0) {
+
+      }
+      else {
+        userNameExists = true
+
+        password = results[0]['password']
+        console.log(password)
+
+      }
+      return results
+    }).catch(function (e) {
+      console.log(e.message);
+      console.log(e.stack);
+    });
+    // res.render('identify.ejs')
+  }
+  console.log("here")
+  if (userNameExists == true) {
+    try {
+      console.log(req.body.password)
+      encryptedpass = await bcrypt.hash(req.body.password, 10)
+      console.log(encryptedpass)
+    } catch {
+      console.log("error")
+    }
+
+    try {
+      if (await bcrypt.compare(req.body.password, password)) {
+        console.log("teet")
+        // var token = jwt.sign(userName, process.env.TOKEN)
+
+        res.render("start.ejs")
+
+
+      } else {
+        res.render("fail.ejs")
+
+      }
+    } catch {
+      console.log("error")
+    }
+  } else {
+
+    res.render("fail.ejs")
+
+  }
 })
 
+
+
 app.get('/identify', (req, res) => {
-  console.log("anam")
   res.render('identify.ejs')
 })
+
+app.get('/admin', (req, res) => {
+  res.render('admin.ejs')
+})
+
+
 
 function authenticateToken(req, res, next) {
   if (currentKey == '') {
@@ -44,6 +103,33 @@ function authenticateToken(req, res, next) {
 
 app.get('/granted', authenticateToken, (req, res) => {
   res.render('start.ejs')
+})
+
+app.post('/register', async (req, res) => {
+
+  if (req.method == 'POST') {
+    encryptedpass = await bcrypt.hash("12345", 10)
+    console.log(encryptedpass)
+    encryptedpass = await bcrypt.hash("12345", 10)
+    console.log(encryptedpass)
+
+    if (req.body.password != '' && req.body.userId != '' && req.body.role != '') {
+      try {
+        dbEncryption = await bcrypt.hash(req.body.password, 10)
+        console.log(req.body.password)
+        console.log(dbEncryption)
+        db.addUserInfo(req.body.userId, dbEncryption, req.body.role);
+        res.render("identify.ejs")
+      } catch {
+        console.log("error")
+      }
+    }
+  }
+
+})
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
 })
 
 app.listen(8000)
